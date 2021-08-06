737b4f8292e8c27beb405299217c6c1827c4e6dc
DimasX <Dimon1998daf@mail.ru>
2021-08-06 17:12:59 +0600

    WorkContr

---
+++ WeatherMonitoring/WeatherMonitoring/Controllers/WeatherForecastController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherMonitoring.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        private readonly WeatherHolder weather;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, WeatherHolder weathers)
+        {
+            _logger = logger;
+            weather = weathers;
+        }
+
+        
+        [HttpPost("save")]
+        public IActionResult Create([FromQuery]int tempF, string date)
+        {
+            weather.Add(tempF, date);
+            return Ok();
+        }
+
+        [HttpGet("read")]
+        public IActionResult Read()
+        {
+            return Ok(weather.Get());
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update([FromQuery] int newtemp, [FromQuery] string newdateTime, [FromQuery] int temp, [FromQuery] string dateTime)
+        {
+            for(int i=0; i < weather.weathers.Count; i++)
+            {
+                if(weather.weathers[i].TemperatureC == temp&& weather.weathers[i].Date == dateTime)
+                {
+                    weather.weathers[i].Date = newdateTime;
+                    weather.weathers[i].TemperatureC = newtemp;
+                }
+            }
+            return Ok();
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromQuery] int tempdel,[FromQuery] string dateTime)
+        {
+            for(int i = 0; i< weather.weathers.Count; i++)
+            {
+                if (weather.weathers[i].Date == dateTime && weather.weathers[i].TemperatureC == tempdel)
+                {
+                    weather.weathers.Remove(weather.weathers[i]);
+                }
+            }
+            return Ok();
+        }
+    }
+}
---
+++ WeatherMonitoring/WeatherMonitoring/WeatherHolder.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherMonitoring
+{
+    public class WeatherHolder
+    {
+        public List<WeatherForecast> weathers { get; set; }
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing",  
+            "Bracing",   
+            "Chilly",    
+            "Cool",     
+            "Mild",     
+            "Warm",     
+            "Balmy",    
+            "Hot",      
+            "Sweltering", 
+            "Scorching"   
+        };
+       
+        
+        public string Summariesverd(int temp)
+        {
+            if (temp < -50)
+            {
+                return Summaries[0];
+            }
+            else if (temp < -30)
+            {
+                return Summaries[1];
+            }
+            else if (temp < -20)
+            {
+                return Summaries[2];
+            }
+            else if (temp < -10)
+            {
+                return Summaries[3];
+            }
+            else if (temp < 0)
+            {
+                return Summaries[4];
+            }
+            else if (10 < temp)
+            {
+                return Summaries[5];
+            }
+            else if (20 < temp)
+            {
+                return Summaries[6];
+            }
+            else if (30 < temp)
+            {
+                return Summaries[7];
+            }
+            else if (40 < temp)
+            {
+                return Summaries[8];
+            }
+            else if (45 < temp)
+            {
+                return Summaries[9];
+            }
+            return null;
+        }
+
+        public WeatherHolder()
+        {
+            var rng = new Random();
+            weathers = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = "",
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToList();
+        }
+
+        public void Add(int temp, string date)
+        {
+
+            var weather = new WeatherForecast
+            {
+                Date = date,
+                TemperatureC = temp,
+                Summary = Summariesverd(temp)
+            };
+            weathers.Add(weather);
+        }
+
+        public List<WeatherForecast> Get()
+        {
+            return weathers;
+        }
+
+    }
+}
