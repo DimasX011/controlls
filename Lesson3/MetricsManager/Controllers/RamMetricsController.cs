f64a72579019bc0cc3a6ec101f3bb75abad2c10c
DimasX011 <Dimon1998daf@mail.ru>
2021-09-27 12:10:41 +0600

    Lesson3

---
+++ Lesson3/MetricsManager/Controllers/RamMetricsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RamMetricsController : ControllerBase
+    {
+        private readonly ILogger<RamMetricsController> _logger;
+        public RamMetricsController(ILogger<RamMetricsController> logger)
+        {
+            _logger = logger;
+            _logger.LogDebug(1, "NLog встроен в CpuMetricsController");
+        }
+    }
+}
---
+++ Lesson3/TestMetricManager/TestAgentMetric/DotnetMetricsControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MetricsAgent.AllRequestMetric;
+using MetricsAgent.Controllers;
+using MetricsAgent.DotnetMetricRepo;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace TestMetricManager.TestAgentMetric
+{
+   public class DotnetMetricsControllerTest
+    {
+        
+        [Fact]
+        public void Create_ShouldCall_Create_From_Repository()
+        {
+            var request = new CreateRequest { Time = TimeSpan.FromSeconds(1), Value = 50 };
+            var reposytorymock = new Mock<IDotnetInterfaceRepository>();
+            var logger = new Mock<ILogger<AgentMetricsController>>();
+            var controller = new DotnetMetricsController(reposytorymock.Object, logger.Object);
+            controller.Create(request);
+            reposytorymock.Verify(repository => repository.Create(It.IsAny<DotnetMetric>()), Times.Once);
+        }
+
+        [Fact]
+        public void GetMetricsCpuMetricAgent()
+        {
+            TimeSpan time = new TimeSpan(12);
+            TimeSpan nexttime = new TimeSpan(19);
+            var reposytorymock = new Mock<IDotnetInterfaceRepository>();
+            var logger = new Mock<ILogger<AgentMetricsController>>();
+            var controller = new DotnetMetricsController(reposytorymock.Object, logger.Object);
+            var result = controller.GetMetricsDotnetMetricsAgent(time, nexttime);
+            Assert.IsAssignableFrom<IActionResult>(result);
+        }
+    }
+}
