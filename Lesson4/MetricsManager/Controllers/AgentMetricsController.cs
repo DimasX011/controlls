baa146186feb35c907e24945c570eb2de8c9a675
DimasX011 <Dimon1998daf@mail.ru>
2021-09-27 15:05:08 +0600

    Lesson4

---
+++ Lesson4/MetricsManager/Controllers/AgentMetricsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgentMetricsController : ControllerBase
+    {
+        private readonly ILogger<AgentMetricsController> _logger;
+        public AgentMetricsController(ILogger<AgentMetricsController> logger)
+        {
+            _logger = logger;
+            _logger.LogDebug(1, "NLog встроен в CpuMetricsController");
+        }
+
+        [HttpPost("register")]
+        public IActionResult RegisterAgent([FromBody] AgentInfo agentInfo)
+        {
+            _logger.LogDebug("Регистрация пользователя:", agentInfo);
+            return Ok();
+           
+        }
+
+        [HttpPut("enable/{agentId}")]
+        public IActionResult EnableAgentById([FromRoute] int agentId)
+        {
+            _logger.LogDebug("Регистрация пользователя:", agentId);
+            return Ok();
+        }
+
+        [HttpPut("disable/{agentId}")]
+        public IActionResult DisableAgentById([FromRoute] int agentId)
+        {
+            _logger.LogDebug("Регистрация пользователя:", agentId);
+            return Ok();
+        }
+
+    }
+}
