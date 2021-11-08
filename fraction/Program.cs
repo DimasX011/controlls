cc67222315e6a1c0ec3658e445e97ae261bf9e3e
DimasX011 <Dimon1998daf@mail.ru>
2021-11-08 17:52:33 +0600

    fraction

---
+++ fraction/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace fraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction fraction = new Fraction(18,21);
+
+            int value = fraction.Denumerator(93, 27);
+
+            int numer = fraction.Numerator(2, 2, 1, 1,"+");
+
+           
+
+            Console.WriteLine(numer);
+        }
+    }
+}
